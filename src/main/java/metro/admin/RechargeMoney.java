de551f5383224fc610a27a226f9389f6ddba572e
AtikAkbar <atik.akbar02@gmail.com>
2023-06-14 05:10:11 +0600

    RechargeMoney Working Properly

--- src/main/java/metro/admin/RechargeMoney.java
+++ src/main/java/metro/admin/RechargeMoney.java
@@ -3,189 +3,102 @@ package metro.admin;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
-public class RechargeMoney extends JFrame implements ActionListener {
-
-    private JTextField usernameField, cardNumberField, rechargeAmountField;
-    private JButton rechargeButton, cancelButton;
-    private String signUpFilePath = "target/files/userInfo/userInfo.txt";
+public class RechargeMoney extends JFrame {
+    private JTextField cardNumberField;
+    private JTextField rechargeAmountField;
 
     public RechargeMoney() {
-        getContentPane().setBackground(Color.WHITE);
-        setLayout(null);
-        setSize(600,400);
+        setTitle("Recharge Program");
+        setSize(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        JLabel title = new JLabel("Recharge Money");
-        title.setBounds(130, 10, 150, 20);
-        title.setForeground(Color.BLACK);
-        title.setFont(new Font("serif", Font.BOLD, 20));
-        add(title);
-
-        JLabel usernameLabel = new JLabel("Username:");
-        usernameLabel.setBounds(50, 50, 80, 20);
-        add(usernameLabel);
-
-        usernameField = new JTextField();
-        usernameField.setBounds(140, 50, 150, 20);
-        add(usernameField);
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 2));
 
         JLabel cardNumberLabel = new JLabel("Card Number:");
-        cardNumberLabel.setBounds(50, 80, 90, 20);
-        add(cardNumberLabel);
-
         cardNumberField = new JTextField();
-        cardNumberField.setBounds(140, 80, 150, 20);
-        add(cardNumberField);
-
         JLabel rechargeAmountLabel = new JLabel("Recharge Amount:");
-        rechargeAmountLabel.setBounds(30, 110, 120, 20);
-        add(rechargeAmountLabel);
-
         rechargeAmountField = new JTextField();
-        rechargeAmountField.setBounds(140, 110, 150, 20);
-        add(rechargeAmountField);
+        JButton rechargeButton = new JButton("Recharge");
+        JButton cancelButton = new JButton("Cancel");
 
-        cancelButton = new JButton("Cancel");
-        cancelButton.setBounds(40, 210, 120, 30);
-        cancelButton.addActionListener(this);
-        cancelButton.setBackground(Color.BLACK);
-        cancelButton.setForeground(Color.WHITE);
-        add(cancelButton);
+        rechargeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String cardNumber = cardNumberField.getText();
+                int rechargeAmount = Integer.parseInt(rechargeAmountField.getText());
 
-        rechargeButton = new JButton("Recharge");
-        rechargeButton.setBounds(140, 160, 100, 30);
-        rechargeButton.addActionListener(this);
-        rechargeButton.setBackground(Color.BLACK);
-        rechargeButton.setForeground(Color.WHITE);
-        add(rechargeButton);
+                recharge(cardNumber, rechargeAmount);
+            }
+        });
 
-        revalidate();
-        repaint();
-        setVisible(true);
+        cancelButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new AdminHome().setVisible(true);
+            }
+        });
+
+        panel.add(cardNumberLabel);
+        panel.add(cardNumberField);
+        panel.add(rechargeAmountLabel);
+        panel.add(rechargeAmountField);
+        panel.add(rechargeButton);
+        panel.add(cancelButton);
+
+        getContentPane().add(panel);
     }
 
-    public void actionPerformed(ActionEvent ae) {
-        if (ae.getSource() == cancelButton) {
-            dispose();
-            new AdminHome();
-        } else if (ae.getSource() == rechargeButton) {
-            String username = usernameField.getText();
-            String cardNumber = cardNumberField.getText();
-            String rechargeAmountText = rechargeAmountField.getText();
+    private void recharge(String cardNumber, int rechargeAmount) {
+        String filename = "target/files/userInfo/userInfo.txt";
+        boolean cardNumberMatch = false;
+        StringBuilder fileContent = new StringBuilder();
 
-            if (username.isEmpty() || cardNumber.isEmpty() || rechargeAmountText.isEmpty()) {
-                JOptionPane.showMessageDialog(this, "Please fill in all the fields.", "Error",
-                        JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            long cardNumberValue;
-            int rechargeAmount;
-
-            try {
-                cardNumberValue = Long.parseLong(cardNumber);
-                rechargeAmount = Integer.parseInt(rechargeAmountText);
-            } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(this, "Invalid card number or recharge amount.", "Error",
-                        JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            if (rechargeAmount <= 0) {
-                JOptionPane.showMessageDialog(this, "Recharge amount should be a positive value.", "Error",
-                        JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            boolean found = false;
-            String line;
-            try (BufferedReader br = new BufferedReader(new FileReader(signUpFilePath))) {
-                while ((line = br.readLine()) != null) {
-                    if (line.contains("username=" + username) && line.contains("cardNumber=" + cardNumberValue)) {
-                        found = true;
-                        break;
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            if (found) {
-                // Add recharge amount to user's account
-                addRechargeAmount(username, cardNumber, rechargeAmount);
-                JOptionPane.showMessageDialog(this, "Recharge successful!", "Success", JOptionPane.INFORMATION_MESSAGE);
-            } else {
-                JOptionPane.showMessageDialog(this, "Invalid username or card number.", "Error",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        }
-    }
-
-    private void addRechargeAmount(String username, String cardNumber, int rechargeAmount) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(signUpFilePath));
-            StringBuilder fileContent = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
             String line;
+
             while ((line = reader.readLine()) != null) {
-                if (line.startsWith("username=") && line.contains("cardNumber=")) {
-                    String savedUsername = line.substring(9);
-                    String cardNumberLine = reader.readLine();
-                    String savedCardNumber = cardNumberLine.substring(11);
-                    if (username.equals(savedUsername) && cardNumber.equals(savedCardNumber)) {
-                        String balanceLine = reader.readLine();
-                        int currentBalance = Integer.parseInt(balanceLine.substring(8));
-                        int newBalance = currentBalance + rechargeAmount;
-                        balanceLine = "balance=" + newBalance;
-                        line = "username=" + savedUsername + "\n" + "cardNumber=" + savedCardNumber + "\n"
-                                + balanceLine;
-                    }
+                String[] parts = line.split("=");
+
+                if (parts[0].equals("cardNumber") && parts[1].equals(cardNumber)) {
+                    cardNumberMatch = true;
+                    int balance = Integer.parseInt(reader.readLine().split("=")[1]);
+
+                    // Update the balance
+                    balance += rechargeAmount;
+                    line = parts[0] + "=" + parts[1]; // Keep the card number line
+                    fileContent.append(line).append(System.lineSeparator());
+
+                    line = "balance=" + balance; // Update the balance line
                 }
-                fileContent.append(line).append("\n");
+
+                fileContent.append(line).append(System.lineSeparator());
             }
 
-            // Write updated content to file
-            try (FileWriter writer = new FileWriter(signUpFilePath)) {
-                writer.write(fileContent.toString());
-            }
             reader.close();
+
+            if (cardNumberMatch) {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+                writer.write(fileContent.toString());
+                writer.close();
+                JOptionPane.showMessageDialog(this, "Recharge successful. Balance updated.");
+            } else {
+                JOptionPane.showMessageDialog(this, "No user found with the specified card number.");
+            }
         } catch (IOException e) {
+            JOptionPane.showMessageDialog(this, "An error occurred while reading or writing the file.");
             e.printStackTrace();
         }
     }
 
-    // private void addRechargeAmount(String username, String cardNumber, int rechargeAmount) {
-    //     try {
-    //         BufferedReader reader = new BufferedReader(new FileReader(signUpFilePath));
-    //         StringBuilder fileContent = new StringBuilder();
-    //         String line;
-    //         while ((line = reader.readLine()) != null) {
-    //             if (line.startsWith("username=" + username) && line.contains("cardNumber=" + cardNumber)) {
-    //                 String[] userDetails = line.split(",");
-    //                 int currentBalance = Integer.parseInt(userDetails[4].substring(userDetails[4].indexOf("=") + 1));
-    //                 int newBalance = currentBalance + rechargeAmount;
-    //                 userDetails[4] = "balance=" + newBalance;
-    //                 line = String.join(",", userDetails);
-    //             }
-    //             fileContent.append(line).append("\n");
-    //         }
-
-    //         // Write updated content to file
-    //         try (FileWriter writer = new FileWriter(signUpFilePath)) {
-    //             writer.write(fileContent.toString());
-    //         }
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    // }
-
     public static void main(String[] args) {
-        new RechargeMoney();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new RechargeMoney().setVisible(true);
+            }
+        });
     }
 }
-
